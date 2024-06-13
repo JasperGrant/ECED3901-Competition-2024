c540485b7052de13693df3254cb2e0b9f9c10af5
JasperGrant <jaspergrant@dal.ca>
2024-06-13 15:17:32 -0300

    Added launch files

---
+++ launch/team_11_launch.py
@@ -0,0 +1,26 @@
+# Launch file to demonstrate namespaces and remapping
+# Written by Jasper Grant
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+	return LaunchDescription([
+		Node(
+			package='eced3901_competition_2024',
+			namespace='team_11_namespace',
+			executable='test_student',
+			name='team_11_robot',
+			arguments='1',
+			remappings=[
+				('/team_11_namespace/team_1_pose', '/team_1_pose'),
+				('/team_11_namespace/team_2_pose', '/team_2_pose'),
+				('/team_11_namespace/CompetitionStart', '/CompetitionStart'),
+				('/team_11_namespace/team_1_ready', '/team_1_ready'),
+				('/team_11_namespace/team_2_ready', '/team_2_ready')
+				
+			],
+			output='screen'
+		)
+	])
+
---
+++ launch/team_15_launch.py
@@ -0,0 +1,25 @@
+# Launch file to demonstrate namespaces and remapping
+# Written by Jasper Grant
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+	return LaunchDescription([
+		Node(
+			package='eced3901_competition_2024',
+			namespace='namespace_team_15',
+			executable='test_student',
+			name='team_15_robot',
+			arguments='2',
+			remappings=[
+				('/namespace_team_15/team_2_pose', '/team_2_pose'),
+				('/namespace_team_15/team_1_pose', '/team_1_pose'),
+				('/namespace_team_15/CompetitionStart', '/CompetitionStart'),
+				('/namespace_team_15/team_1_ready', '/team_1_ready'),
+				('/namespace_team_15/team_2_ready', '/team_2_ready')
+			],
+			output='screen'
+		)
+	])
+
