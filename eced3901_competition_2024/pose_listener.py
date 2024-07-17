34d46604648636540cfa001f8ed4d6e24e2193a9
Jasper Grant <jasperbengrant@gmail.com>
2024-07-17 09:55:38 -0300

    Added UDP to pose_listener

--- eced3901_competition_2024/pose_listener.py
+++ eced3901_competition_2024/pose_listener.py
@@ -6,7 +6,12 @@
 import rclpy
 from rclpy.node import Node
 
+import socket
+
 from geometry_msgs.msg import Pose
+
+udp_ip = "10.0.0.230"
+udp_port = 9090
 
 # Class to print to console every pose update
 class PoseListener(Node):
@@ -31,11 +36,39 @@ class PoseListener(Node):
 	# Callback for team 1's pose updates
 	def team_1_callback(self, msg):
 		self.get_logger().info(f"Team 1 position: {msg.position.x},{msg.position.y},{msg.position.z}, orientation: {msg.orientation.x},{msg.orientation.y},{msg.orientation.z},{msg.orientation.w}\n")
+		# Send UDP packet to main system in json format
+		# Format:
+		# {
+		# 	"team": 1,
+		# 	"x": msg.position.x,
+		# 	"y": msg.position.y,
+		# 	"theta": msg.orientation.z,
+		# }
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+		data = {
+			"team": 1,
+			"x": msg.position.x,
+			"y": msg.position.y,
+			"theta": msg.orientation.z,
+		}
+		sock.sendto(str(data).encode(), (udp_ip, udp_port))
+		
 		
 	# Callback for team 2's pose updates
 	def team_2_callback(self, msg):
 		# This will be flipped in the actual competition but is taken as is now.
 		self.get_logger().info(f"Team 2 position: {msg.position.x},{msg.position.y},{msg.position.z}, orientation: {msg.orientation.x},{msg.orientation.y},{msg.orientation.z},{msg.orientation.w}\n")
+		# Send UDP packet to main system in json format
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+		data = {
+			"team": 2,
+			"x": msg.position.x,
+			"y": msg.position.y,
+			"theta": msg.orientation.z,
+		}
+		sock.sendto(str(data).encode(), (udp_ip, udp_port))
 
 		
 def main():
